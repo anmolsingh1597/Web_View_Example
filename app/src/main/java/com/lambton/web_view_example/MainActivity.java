4cfd0bf288d8ce119031e0ad444172e675b8cdf5
Anmol Singh <anmolsingh1597@gmail.com>
2020-04-14 18:27:10 -0400

    [Anmol Singh]WebView: WebPage fetched from within Assets folder

--- app/src/main/java/com/lambton/web_view_example/MainActivity.java
+++ app/src/main/java/com/lambton/web_view_example/MainActivity.java
@@ -2,8 +2,14 @@ package com.lambton.web_view_example;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.AssetManager;
 import android.os.Bundle;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,8 +24,27 @@ public class MainActivity extends AppCompatActivity {
 
         String webHtml = "<h1>Hello World</h1>";
         webView = findViewById(R.id.webViewId);
-        webView.loadData(webHtml,"text/html","utf8");
 
+        // Load from Html String Data
+//        webView.loadData(webHtml,"text/html","utf8");
 
+        // Load from URL
+//        webView.loadUrl("https://www.google.com");
+//        webView.setWebViewClient(new WebViewClient());
+
+        // Load from Assets
+        AssetManager assetManager = this.getAssets();
+        try{
+            InputStream inputStream = assetManager.open("test.html");
+            int size = inputStream.available();
+            byte[] buffer = new byte[size];
+            inputStream.read(buffer);
+            inputStream.close();
+            String content = new String(buffer, StandardCharsets.UTF_8);
+
+            webView.loadData(content,"text/html","utf-8");
+        }catch (IOException e){
+            e.printStackTrace();
+        }
     }
 }
